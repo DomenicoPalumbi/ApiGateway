b2bc96e8f6b47554bd3ed8f2fbc8a240d1d3971d
DomenicoPalumbi <d.palumbi@elitesoftwarehouse.com>
2025-06-12 11:44:47 +0200

    Autenticazione funzionante

--- src/main/java/com/elitesoftwarehouse/apiGateway/config/SecurityConfig.java
+++ src/main/java/com/elitesoftwarehouse/apiGateway/config/SecurityConfig.java
@@ -4,9 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -16,37 +13,21 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .authorizeExchange(exchanges -> exchanges
-                        .pathMatchers("/login").permitAll()
-                        .pathMatchers("/admin/**").hasAnyRole("USER","ADMIN")
-                        .pathMatchers("/user/**").hasRole("USER")
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/register", "/users").permitAll()
+                        .pathMatchers("/admin/**").permitAll()
                         .anyExchange().authenticated()
                 )
-                .httpBasic(httpBasic -> {})
+                .httpBasic()
+                .and()
                 .build();
     }
+
     @Bean
-    public MapReactiveUserDetailsService userDetailsService(PasswordEncoder encoder) {
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(encoder.encode("admin123"))
-                .roles("ADMIN")  // equivale a ROLE_ADMIN
-                .build();
-
-        UserDetails user = User.builder()
-                .username("user")
-                .password(encoder.encode("user123"))
-                .roles("USER")  // equivale a ROLE_USER
-                .build();
-
-        return new MapReactiveUserDetailsService(admin, user);
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
---
+++ src/main/java/com/elitesoftwarehouse/apiGateway/service/UserService.java
@@ -0,0 +1,28 @@
+package com.elitesoftwarehouse.apiGateway.service;
+import com.elitesoftwarehouse.apiGateway.model.entity.User;
+import com.elitesoftwarehouse.apiGateway.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    public boolean existsByUsername(String username) {
+        return userRepository.existsByUsername(username);
+    }
+
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+}
